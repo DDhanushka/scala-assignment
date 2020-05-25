65e0d37e65525700030cc336ed6546f4be582e71
ddhanushka <dimuthudhanushka8@gmail.com>
2020-05-25 12:17:17 +0530

    Assignment-3 Question:1

--- assignment-3/src/main/scala/Main.scala
+++ assignment-3/src/main/scala/Main.scala
@@ -3,39 +3,23 @@
 object Main extends App {
 	//// 1.	///////////////////////////////////////////////////
 
-	def takeHomeSalaray(wh:Int, oh:Int) :Double= {
-		return income(wh, oh) - tax(income(wh, oh), 10);
+	def prime(n:Int, i:Int=2) :Boolean = {
+		if(i<=n) {
+			if(n%i==0) {
+				if(n==2)
+					return true;
+				else 
+					return false;
+			} else {
+				return true;
+			}
+			return prime(n, i+1);
+		} else 
+		return false;
 	}
-
-	def income(wh:Int, oh:Int) :Int = {
-		return workingHourSalaray(wh, 150) + otHourSalary(oh, 75);
-	}
-
-	def tax(income:Int, tax:Int) :Double = {
-		return income * tax * 0.01;
-	}
-
-	def workingHourSalaray(wh:Int, n:Int) :Int = {
-		return wh * n;
-	}
-
-	def otHourSalary(oh:Int, n:Int) :Int = {
-		return oh * n;
-	}
-
-	println("Take home salaray = " + takeHomeSalaray(40, 20));
+	println("5 "+prime(5));
+	println("8 " +prime(8));
 
 	//// 2.	///////////////////////////////////////////////////
-	
-	def profit(ticketPrice:Int) :Int = income(ticketPrice) - costs(ticketPrice);
 
-	def income(ticketPrice:Int) :Int = attendees(ticketPrice) * ticketPrice;
-
-	def attendees(ticketPrice:Int) :Int = {
-		return 120 + 4 * (15-ticketPrice);
-	}
-
-	def costs(ticketPrice:Int) :Int = 500 + 3 * attendees(ticketPrice);
-
-	println("Profit when ticket price is " + 15 + " = " + profit(20));
 }
